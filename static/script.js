df0e0a9ea85c57f8acaa1fc0102267da90657d1d
kouhei25k <kouhei25k@gmail.com>
2020-04-30 11:11:31 +0900

    fix2 responsive-s

--- static/script.js
+++ static/script.js
@@ -1,9 +1,20 @@
 $(".navbar-toggler").click(function () {
-  console.log("おした！");
+  //   console.log("おした！");
   $("#bg-bk").toggleClass("bg-bk");
   $("#main").toggleClass("blur");
   $(".nav-item>a").toggleClass("c-w");
   $(".nav-item>a:after").toggleClass("bg-w");
   $(".navbar-toggler>.navbar-toggler-icon").toggleClass("navbar-icon-w");
+});
+
+$(document).click(function () {
+  // クリックイベントが発生すると毎回ここ通る
+  if ($(event.target).closest(".nav-link").length) {
+    console.log("ナビがクリックされました");
+  } else if ($(event.target).closest(".bg-bk,.show").length) {
+    $(".navbar-toggler").trigger("click");
+  } else {
+    console.log("関係ないところがクリックされました");
+  }
 });
 // .nav-item >a{color:#fff;} .nav-item>a:after{background-color:#fff;}
